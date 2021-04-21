9152cc0893c1eeb844a8826c729943f4dc09c0da
kvanst <kvanst3@gmail.com>
2021-04-21 22:25:38 +0200

    adds catch on user shift input

--- ceasar cypher.py
+++ ceasar cypher.py
@@ -5,37 +5,41 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
-result = []
+
+if shift > 26:
+    shift = shift % 26
 
 
 def encrypt(text, shift):
+    result = ''
     for letter in text:
         if letter in alphabet:
             letter_position = alphabet.index(letter)
 
             if (letter_position) + shift > 26:
                 j = (letter_position - 26) + shift
-                result.append(alphabet[j])
+                result += alphabet[j]
             else:
-                result.append(alphabet[letter_position + shift])
+                result += alphabet[letter_position + shift]
         else:
-            result.append(letter)
+            result += letter
     return result
 
 
 def decrypt(text, shift):
+    result = ''
     for letter in text:
         if letter in alphabet:
             letter_position = alphabet.index(letter)
-            result.append(alphabet[letter_position - shift])
+            result += alphabet[letter_position - shift]
         else:
-            result.append(letter)
+            result += letter
     return result
 
 
 if direction == 'encode':
-    print(''.join(encrypt(text, shift)))
+    print(encrypt(text, shift))
 elif direction == 'decode':
-    print(''.join(decrypt(text, shift)))
+    print(decrypt(text, shift))
 else:
     print('input error')
