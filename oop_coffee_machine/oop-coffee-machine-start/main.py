54c18f04ee31fc540b8484df0377a56b25dae0b7
kvanst <kvanst3@gmail.com>
2021-04-29 20:19:17 +0200

    refacto python naming convention

--- oop_coffee_machine/oop-coffee-machine-start/main.py
+++ oop_coffee_machine/oop-coffee-machine-start/main.py
@@ -7,25 +7,25 @@ def PowerOn():
     order = input(f"What would you like? {menu.get_items()}: ")
     if order in admin_keywords:
         if order == "report":
-            coffeemaker.report()
-            moneymachine.report()
+            coffee_maker.report()
+            money_machine.report()
             PowerOn()
         else:
             return
     else:
         drink = menu.find_drink(order)
         if drink is not None:
-            if coffeemaker.is_resource_sufficient(drink):
+            if coffee_maker.is_resource_sufficient(drink):
                 print("$", drink.cost)
-                moneymachine.make_payment(drink.cost)
-                coffeemaker.make_coffee(drink)
+                money_machine.make_payment(drink.cost)
+                coffee_maker.make_coffee(drink)
                 PowerOn()
         else:
             PowerOn()
 
 
 menu = Menu()
-coffeemaker = CoffeeMaker()
-moneymachine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
 admin_keywords = ["off", "report"]
 PowerOn()
