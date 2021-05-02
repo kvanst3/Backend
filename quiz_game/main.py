be08983c27d8ee7a50afdca9457c0cd9e133ec00
kvanst <kvanst3@gmail.com>
2021-05-02 19:28:26 +0200

    oop_quiz_game

---
+++ quiz_game/main.py
@@ -0,0 +1,19 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import Quiz
+
+
+question_bank = []
+for question in question_data:
+    new_question = Question(question['text'], question['answer'])
+    question_bank.append(new_question)
+
+
+quiz = Quiz(question_bank)
+
+
+while quiz.question_number < 12:
+    print(f"Current Score: {quiz.right_answers}/{quiz.question_number}")
+    quiz.ask_question()
+
+print(f"Your Final Score is: {quiz.right_answers}/12")
---
+++ quiz_game/quiz_brain.py
@@ -0,0 +1,21 @@
+class Quiz():
+
+    def __init__(self, q_bank):
+        self.question_bank = q_bank
+        self.user_answer = bool()
+        self.question_number = 0
+        self.right_answers = 0
+
+    def ask_question(self):
+        question = self.question_bank[self.question_number]
+        self.user_answer = True if input(question.q_text) == 't' else False
+        self.check_answer()
+        self.question_number += 1
+
+    def check_answer(self):
+        question = self.question_bank[self.question_number]
+        if question.q_answer == str(self.user_answer):
+            self.right_answers += 1
+            print("Seikou!\n")
+        else:
+            print("Batsu...\n")
