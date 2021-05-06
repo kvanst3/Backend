03c0eca960a28141917e173fa1e0e05b588dfce5
kvanst <kvanst3@gmail.com>
2021-05-06 22:22:51 +0200

    food can be added to screen

---
+++ snake_game/food.py
@@ -0,0 +1,18 @@
+from turtle import Turtle
+import random
+
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=0.3, stretch_wid=0.3)
+        self.color("white")
+        self.speed("fastest")
+
+    def refresh(self):
+        random_x = random.randrange(-290, 290, 10)
+        random_y = random.randrange(-290, 290, 10)
+        self.goto(random_x, random_y)
