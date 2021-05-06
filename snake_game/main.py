224996ab79383a7ee592155ffee38c34bf5eae9b
kvanst <kvanst3@gmail.com>
2021-05-06 22:23:21 +0200

    snake grows in size when eating food

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 
@@ -10,17 +11,22 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 screen.update()
+
 screen.listen()
+screen.onkey(snake.turn_left, "a")
+screen.onkey(snake.turn_right, "d")
 
 game_on = True
 
 while game_on:
-    screen.onkey(snake.turn_left, "a")
-    screen.onkey(snake.turn_right, "d")
     time.sleep(.1)
     screen.update()
     snake.move()
+    if snake.head.distance(food) < 10:
+        food.refresh()
+        snake.add_segment()
 
 
 
--- snake_game/snake.py
+++ snake_game/snake.py
@@ -1,23 +1,26 @@
 from turtle import Turtle
 
 
-class Snake():
+class Snake:
 
     def __init__(self):
         self.segments = []
         self.generate_snake()
         self.head = self.segments[0]
 
+    def add_segment(self):
+        segment = Turtle()
+        segment.penup()
+        segment.shape("square")
+        segment.shapesize(stretch_wid=0.5, stretch_len=0.5, outline=0)
+        segment.color("white")
+        if len(self.segments) > 0:
+            segment.goto(self.segments[-1].xcor() - 11, self.segments[-1].ycor())
+        self.segments.append(segment)
+
     def generate_snake(self):
         for _ in range(3):
-            snake = Turtle()
-            snake.penup()
-            snake.shape("square")
-            snake.shapesize(stretch_wid=0.5, stretch_len=0.5, outline=0.1)
-            snake.color("white")
-            if len(self.segments) > 0:
-                snake.goto(self.segments[-1].xcor() - 11, self.segments[-1].ycor())
-            self.segments.append(snake)
+            self.add_segment()
 
     def move(self):
         # range(start=, stop=, step=) from 2 to(not including) 0, by -1
