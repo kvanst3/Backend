55f91aa30d54b3841cf845322953a9b713bee54d
kvanst <kvanst3@gmail.com>
2021-05-06 22:28:23 +0200

    add a refresh function to food class

--- snake_game/food.py
+++ snake_game/food.py
@@ -11,7 +11,8 @@ class Food(Turtle):
         self.shapesize(stretch_len=0.3, stretch_wid=0.3)
         self.color("white")
         self.speed("fastest")
-
+        self.refresh()
+        
     def refresh(self):
         random_x = random.randrange(-290, 290, 10)
         random_y = random.randrange(-290, 290, 10)
