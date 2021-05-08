9545ef99467d8a79b80e9b64db3bf7e4fc2c313d
kvanst <kvanst3@gmail.com>
2021-05-09 00:07:54 +0200

    add ball to the screen and moves top right

--- pong/main.py
+++ pong/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.setup(height=600, width=800)
@@ -9,6 +11,7 @@ screen.tracer(0)
 
 player1 = Paddle((-365,0))
 player2 = Paddle((365, 0))
+ball = Ball()
 
 screen.listen()
 screen.onkey(player1.move_up, "w")
@@ -19,8 +22,8 @@ screen.onkey(player2.move_down, "Down")
 game_on = True
 while game_on:
     screen.update()
-
-
+    ball.move()
+    time.sleep(.06)
 
 
 screen.exitonclick()
