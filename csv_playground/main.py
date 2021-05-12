dec014443bcbd9773bdfceabd28c4f8252d82c37
kvanst <kvanst3@gmail.com>
2021-05-12 21:58:39 +0200

    get data all fashion way

---
+++ csv_playground/main.py
@@ -0,0 +1,7 @@
+# with open("csv_playground/weather_data.csv") as file:
+#     content = []
+#     for line in file.readlines():
+#         content.append(line.strip().split(','))
+
+# print(content)
+
