2fd2fae26956504dd7fa6115af4ff59c452d8c90
kvanst <kvanst3@gmail.com>
2021-05-12 21:59:13 +0200

    get data with csv module

--- csv_playground/main.py
+++ csv_playground/main.py
@@ -5,3 +5,13 @@
 
 # print(content)
 
+# import csv
+
+# with open("csv_playground/weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temps = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temps.append(int(row[1]))
+
+# print(temps)
