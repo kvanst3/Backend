7f602838a39522f9caf5beadb3aa7616fd26c8d4
kvanst <kvanst3@gmail.com>
2021-05-13 12:02:40 +0200

    manipulate big data

--- csv_playground/main.py
+++ csv_playground/main.py
@@ -16,7 +16,7 @@
 
 # print(temps)
 
-# import pandas
+import pandas
 # from pandas.core.frame import DataFrame
 
 # data = pandas.read_csv("csv_playground/weather_data.csv")
@@ -31,14 +31,26 @@
 # print(data[data.temp == data.temp.max()])
 
 ## create a DataFrame
-import pandas
 
+# data_dict = {
+#     "fruits": ["Apple", "Pears", "Strawberry", "Blueberry"],
+#     "amount": [5, 7, 15, 32]
+# }
+
+# data = pandas.DataFrame(data_dict)
+# print(data)
+# data.to_csv("csv_playground/fruits.csv")
+
+data = pandas.read_csv("csv_playground/2018_central_park_squirrel_census_squirrel_data.csv")
+fur_data = data["Primary Fur Color"]
+
+red = fur_data[fur_data == "Cinnamon"].count()
+grey = fur_data[fur_data == "Gray"].count()
+black = fur_data[fur_data == "Black"].count()
 
 data_dict = {
-    "fruits": ["Apple", "Pears", "Strawberry", "Blueberry"],
-    "amount": [5, 7, 15, 32]
+    "fur color": ["Cinnamon", "Gray", "Black"],
+    "Squirrel population": [red, grey, black]
 }
-
-data = pandas.DataFrame(data_dict)
-print(data)
-data.to_csv("csv_playground/fruits.csv")
+data_framed_data = pandas.DataFrame(data_dict)
+data_framed_data.to_csv("csv_playground/squirrel_count.csv")
