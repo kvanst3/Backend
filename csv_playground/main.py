87c7fd72bcff18ac4a776da8059158fd0bdd3081
kvanst <kvanst3@gmail.com>
2021-05-13 11:25:12 +0200

    create data frame

--- csv_playground/main.py
+++ csv_playground/main.py
@@ -16,8 +16,29 @@
 
 # print(temps)
 
+# import pandas
+# from pandas.core.frame import DataFrame
+
+# data = pandas.read_csv("csv_playground/weather_data.csv")
+# print(data)
+# print(data["temp"])
+
+# print(data.temp)
+# print(data.temp.mean())
+# print(data.temp.max())
+# # get a row
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max()])
+
+## create a DataFrame
 import pandas
 
-data = pandas.read_csv("csv_playground/weather_data.csv")
+
+data_dict = {
+    "fruits": ["Apple", "Pears", "Strawberry", "Blueberry"],
+    "amount": [5, 7, 15, 32]
+}
+
+data = pandas.DataFrame(data_dict)
 print(data)
-print(data["temp"])
+data.to_csv("csv_playground/fruits.csv")
