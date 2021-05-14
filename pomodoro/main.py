e1e1366a49cf3d8318278dffba394af02064b96a
kvanst <kvanst3@gmail.com>
2021-05-14 23:13:46 +0200

    all widgets initialized

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -26,8 +26,28 @@ canvas.create_image(100, 112, image=tomato_image)
 canvas.create_text(100, 145, text="00:00", fill="white", font=(FONT_NAME, 32, "bold"))
 canvas.grid(column=2, row=2)
 
-timer_label = Label(text="Timer", font=(FONT_NAME, 32, "bold"), fg=GREEN, bg=YELLOW)
+# Labels
+reset_num = 1
+
+timer_label = Label(text="Timer", font=(FONT_NAME, 50), fg=GREEN, bg=YELLOW)
 timer_label.grid(column=2, row=1)
+
+check_label = Label(text=reset_num * "✔", font=(FONT_NAME, 20, "bold"), fg=GREEN, bg=YELLOW)
+check_label.grid(column=2, row=4)
+
+# Buttons
+
+def start_action():
+    print("start")
+
+def reset_action():
+    reset_num += 1
+
+start_bt = Button(text="start", command=start_action, bg="white", highlightthickness=0)
+start_bt.grid(column=1, row=3)
+
+reset_bt = Button(text="reset", command=reset_action, bg="white", highlightthickness=0)
+reset_bt.grid(column=3, row=3)
 
 
 window.mainloop()
