b456a4170c245c8d4744bbf2cfc8cf0d2e0ebd08
kvanst <kvanst3@gmail.com>
2021-05-15 22:59:50 +0200

    create labels

--- password_manager/main.py
+++ password_manager/main.py
@@ -11,7 +11,16 @@ window.config(padx=20, pady=20, bg="white")
 canvas = Canvas(width=200, height=200, bg="white", highlightthickness=0)
 img = PhotoImage(file="/home/k/Desktop/Python_projects/Python projects/password_manager/logo.png")
 canvas.create_image(100, 100, image=img)
-canvas.grid(column=2, row=2)
+canvas.grid(column=2, row=1)
+
+website_label = Label(text="Website", bg="white")
+website_label.grid(column=1, row=2)
+
+usr_label = Label(text="Email/Username", bg="white")
+usr_label.grid(column=1, row=3)
+
+password_label = Label(text="Password", bg="white")
+password_label.grid(column=1, row=4)
 
 
 
