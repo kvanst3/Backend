779e175dbc9e6103df0a3ad7d52c5f37345b0b4c
kvanst <kvanst3@gmail.com>
2021-05-16 13:03:26 +0200

    hiss

--- password_manager/main.py
+++ password_manager/main.py
@@ -1,13 +1,46 @@
 from tkinter import *
+from tkinter import messagebox
+from random import choice, randint, shuffle
 
 FONT = 10
+LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+SYMBOLS = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
-    print("generating...")
+    password_entry.delete(0, END)
+
+    password_letters = [choice(LETTERS) for _ in range(randint(8, 10))]
+    password_numbers = [choice(NUMBERS) for _ in range(randint(2, 4))]
+    password_symbols = [choice(SYMBOLS) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_numbers + password_symbols
+
+    shuffle(password_list)
+
+    password = ""
+    for char in password_list:
+        password += char
+
+    password_entry.insert(0, password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
-    print("saving...")
+    web = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    if len(web) > 0 and len(password) > 0:
+        save_message = messagebox.askokcancel(title=web, message=f"Details:\nEmail: {email}\nPassword: {password}")
+        if save_message:
+            with open("password_manager/passwords.txt", mode="a") as file:
+                file.write(f"{web}, {email}, {password}\n")
+
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+    else:
+        messagebox.showerror(title="Error", message= "You need to populate all the fields before hitting Add")
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -22,18 +55,25 @@ canvas.grid(column=1, row=0)
 # Labels
 website_label = Label(text="Website:", bg="white", pady=5, font=("Arial", FONT))
 website_label.grid(column=0, row=1)
+
 email_label = Label(text="Email/Username:", bg="white", pady=5, font=("Arial", FONT))
 email_label.grid(column=0, row=2)
+
 password_label = Label(text="Password:", bg="white", pady=5, font=("Arial", FONT))
 password_label.grid(column=0, row=3)
+
 fake_label = Label(text=" ", bg="white", pady=5, font=("Arial", FONT))
 fake_label.grid(column=0, row=4)
 
 # Entries
 website_entry = Entry(width=36)
 website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
+
 email_entry = Entry(width=36)
 email_entry.grid(column=1, row=2, columnspan=2)
+email_entry.insert(0, "email@email.com")
+
 password_entry = Entry(width=24)
 password_entry.grid(column=1, row=3)
 
