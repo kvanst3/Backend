4c949a6d162b69af3ae11cb43f4db846f478b633
kvanst <kvanst3@gmail.com>
2021-05-17 13:46:25 +0200

    program runs recursively

--- nato_alphabet/main.py
+++ nato_alphabet/main.py
@@ -21,16 +21,20 @@ import pandas
 # {new_key:new_value for (index, row) in df.iterrows()}
 
 #TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+#{"A": "Alfa", "B": "Bravo"}
 df = pandas.read_csv("nato_alphabet/nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for (index, row) in df.iterrows()}
 
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-spell = input("What would you like to spell out?\n").upper()
-try:    
-    result = [nato_dict[char] for char in spell]
-except KeyError as e:
-    result = f"Sorry, {e} is not a letter and can't, therefore, be translated by our program.."
-finally:
-    print(result)
+def run_program():
+    spell = input("What would you like to spell out?\n").upper()
+    try:    
+        result = [nato_dict[char] for char in spell]
+    except KeyError as e:
+        result = f"Sorry, {e} is not a letter and can't, therefore, be translated by our program.."
+    finally:
+        print(result)
+        run_program()
+
+run_program()
