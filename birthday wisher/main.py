455cb2df7e889253191f4ba693576a9c55b322da
kvanst <kvanst3@gmail.com>
2021-05-20 11:21:09 +0200

    will send simple email

--- birthday wisher/main.py
+++ birthday wisher/main.py
@@ -1,8 +1,14 @@
+from email import message
 import smtplib
 from user_info import my_email, my_password
 
 
 
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=my_email, )
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=my_password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="charlesgroin@gmail.com",
+        msg="Subject:Happy Birthday!\n\nHello from python"
+    )
