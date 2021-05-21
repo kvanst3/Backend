586ce395079b732a97ad0ba779674306cf43d116
kvanst <kvanst3@gmail.com>
2021-05-21 21:38:56 +0200

    refacto

--- quiz_game/main.py
+++ quiz_game/main.py
@@ -10,9 +10,10 @@ params = {
 }
 response = requests.get("https://opentdb.com/api.php", params=params)
 response.raise_for_status()
+data = response.json()['results']
 
 question_bank = []
-for i in response.json()['results']:
+for i in data:
     new_question = Question(i['question'], i['correct_answer'])
     question_bank.append(new_question)
 
