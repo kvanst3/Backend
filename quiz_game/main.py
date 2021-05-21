d2b1de9215e191de9d2af27aeb7b6c4e191fa8b7
kvanst <kvanst3@gmail.com>
2021-05-21 22:40:39 +0200

    set up the ui complely

--- quiz_game/main.py
+++ quiz_game/main.py
@@ -2,6 +2,7 @@ from question_model import Question
 from data import question_data
 from quiz_brain import Quiz
 import requests
+from ui import QuizInterface
 
 
 params = {
@@ -18,8 +19,10 @@ for i in data:
     question_bank.append(new_question)
 
 
+quiz_ui = QuizInterface()
+
 quiz = Quiz(question_bank)
 
-quiz.ask_question()
+# quiz.ask_question()
 
 print(f"Your Final Score is: {quiz.right_answers}/12")
--- quiz_game/ui.py
+++ quiz_game/ui.py
@@ -1,1 +1,39 @@
+from tkinter import *
+
+
 THEME_COLOR = "#375362"
+
+
+class QuizInterface:
+
+    def __init__(self):
+        self.score = 0
+
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+        
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question = self.canvas.create_text(150, 125, text= "My question here", font=("Arial", 20, "italic"))
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50, padx=0)
+
+        green_img = PhotoImage(file="quiz_game/images/true.png")
+        self.green_button = Button(image=green_img, highlightthickness=0, command=self.is_true)
+        self.green_button.grid(row=2, column=0)
+
+        red_img = PhotoImage(file="quiz_game/images/false.png")
+        self.red_button = Button(image=red_img, highlightthickness=0, command=self.is_false)
+        self.red_button.grid(row=2, column=1)
+
+        self.score_label = Label(text=f"Score: {self.score}", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
+        self.window.mainloop()
+
+    def is_true(self):
+        print("true?")
+    
+    def is_false(self):
+        print("false?")
+
+    def increase_score(self):
+        self.score += 1
