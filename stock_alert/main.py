e40a2f97f69d2181f190c5460a7b445730f19277
kvanst <kvanst3@gmail.com>
2021-05-24 13:13:20 +0200

    get data from newsapi

--- stock_alert/main.py
+++ stock_alert/main.py
@@ -4,10 +4,13 @@ from twilio.rest import Client
 import json
 
 
+NAME = 'nvidia'
 STOCK = "NVDA"
 COMPANY_NAME = "NVIDIA Corp."
 AA_ENDPOINT = "https://www.alphavantage.co/query"
 AA_KEY = os.environ.get('AA_KEY')
+NA_ENDPOINT= "https://newsapi.org/v2/everything"
+NA_KEY = os.environ.get('NEWSAPI_KEY')
 
 
 def get_change(current, previous):
@@ -37,15 +40,23 @@ ototoi_price = float(data["Time Series (Daily)"][list(data["Time Series (Daily)"
 # with open("stock_alert/onecall.json", mode="w") as file:
 #     json.dump(data, file, indent=4)
 
-if get_change(kinou_price, ototoi_price) > 5:
-    print("Get News")
+# if get_change(kinou_price, ototoi_price) > 5:
+params = {
+    'q': NAME,
+    'language': "en",
+    'sort_by': "publishedAt",
+    'pageSize': '3',
+    'apiKey': NA_KEY,
+}
+
+response = requests.get(NA_ENDPOINT, params=params)
+response.raise_for_status()
+
+data = response.json()
 
 
-## STEP 1: Use https://www.alphavantage.co key=RSSTIXHMD8CC71PH
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
-## STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+
 
 ## STEP 3: Use https://www.twilio.com
 # Send a seperate message with the percentage change and each article's title and description to your phone number. 
