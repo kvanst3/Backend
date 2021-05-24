3fe87af49507640e4c163b9862df4e17daa0579c
kvanst <kvanst3@gmail.com>
2021-05-24 12:52:35 +0200

    fetch last 2 price point for given stock

--- stock_alert/main.py
+++ stock_alert/main.py
@@ -6,16 +6,40 @@ import json
 
 STOCK = "NVDA"
 COMPANY_NAME = "NVIDIA Corp."
-AA_ENDPOINT = https://www.alphavantage.co/query
+AA_ENDPOINT = "https://www.alphavantage.co/query"
 AA_KEY = os.environ.get('AA_KEY')
+
+
+def get_change(current, previous):
+    if current == previous:
+        return 0
+    try:
+        return round((abs(current - previous) / previous) * 100.0, 2)
+    except ZeroDivisionError:
+        return 0
 
 
 params = {
     'function': 'TIME_SERIES_DAILY',
-    'symbol': 'NVDA',
-    'outputsize': 'full',
+    'symbol': STOCK,
+    'outputsize': 'compact',
     'apikey': AA_KEY,
 }
+
+response = requests.get(AA_ENDPOINT, params=params)
+response.raise_for_status()
+
+data = response.json()
+
+kinou_price = float(data["Time Series (Daily)"][list(data["Time Series (Daily)"].keys())[0]]["4. close"])
+ototoi_price = float(data["Time Series (Daily)"][list(data["Time Series (Daily)"].keys())[1]]["4. close"])
+
+# with open("stock_alert/onecall.json", mode="w") as file:
+#     json.dump(data, file, indent=4)
+
+if get_change(kinou_price, ototoi_price) > 5:
+    print("Get News")
+
 
 ## STEP 1: Use https://www.alphavantage.co key=RSSTIXHMD8CC71PH
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
