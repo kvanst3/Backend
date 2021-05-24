c49161fcb8d232b02c79008af273e8a82b471d10
kvanst <kvanst3@gmail.com>
2021-05-24 13:40:46 +0200

    permit twilio to send sms if price fluctuation exceeds 5%. Sms includes 3 most recent news articles about company

--- stock_alert/main.py
+++ stock_alert/main.py
@@ -11,13 +11,16 @@ AA_ENDPOINT = "https://www.alphavantage.co/query"
 AA_KEY = os.environ.get('AA_KEY')
 NA_ENDPOINT= "https://newsapi.org/v2/everything"
 NA_KEY = os.environ.get('NEWSAPI_KEY')
+TWILIO_SID = os.environ['TWILIO_SID']
+TWILIO_AUTH = os.environ['TWILIO_AUTH']
+MY_TEL = os.environ['MY_TEL']
 
 
 def get_change(current, previous):
     if current == previous:
         return 0
     try:
-        return round((abs(current - previous) / previous) * 100.0, 2)
+        return round((current - previous) / previous * 100.0, 2)
     except ZeroDivisionError:
         return 0
 
@@ -40,36 +43,31 @@ ototoi_price = float(data["Time Series (Daily)"][list(data["Time Series (Daily)"
 # with open("stock_alert/onecall.json", mode="w") as file:
 #     json.dump(data, file, indent=4)
 
-# if get_change(kinou_price, ototoi_price) > 5:
-params = {
-    'q': NAME,
-    'language': "en",
-    'sort_by': "publishedAt",
-    'pageSize': '3',
-    'apiKey': NA_KEY,
-}
+percent_change = get_change(ototoi_price, kinou_price)
 
-response = requests.get(NA_ENDPOINT, params=params)
-response.raise_for_status()
+if abs(percent_change) > 5:
+    params = {
+        'q': NAME,
+        'language': "en",
+        'sort_by': "publishedAt",
+        'pageSize': '3',
+        'apiKey': NA_KEY,
+    }
 
-data = response.json()
+    response = requests.get(NA_ENDPOINT, params=params)
+    response.raise_for_status()
 
+    data = response.json()
+    up = '🔺'
+    down = '🔻'
 
+    for i in range(3):
+        client = Client(TWILIO_SID, TWILIO_AUTH)
+        message = client.messages \
+            .create(
+                body=f"{STOCK}: {up if percent_change <0 else down}{abs(percent_change)}%\nHeadlines: {data['articles'][i]['title']}\nBrief: {data['articles'][i]['content']}",
+                from_='+17149055219',
+                to=MY_TEL
+            )
 
-
-
-## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number. 
-
-
-#Optional: Format the SMS message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
-
+        print(message.status)
