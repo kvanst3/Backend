7bbef48ecf88df16aa1d10a4cf47c1b866705ab6
kvanst <kvanst3@gmail.com>
2021-05-31 21:51:48 +0200

    get text via target

--- beautiful_soup/main.py
+++ beautiful_soup/main.py
@@ -4,4 +4,15 @@ with open('beautiful_soup/website.html') as file:
     content = file.read()
 
 soup = BeautifulSoup(content, "html.parser")
-print(soup)
+
+all_anchor_tags = soup.find_all(name="a")
+
+for tag in all_anchor_tags:
+    # print(tag.getText())
+    print(tag.get("href"))
+
+heading = soup.find(name="h1", id="name")
+print(heading)
+
+section_heading = soup.find(name="h3", class_="heading")
+print(section_heading.getText())
