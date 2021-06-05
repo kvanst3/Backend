7658f7fef7fdbc4fa91e11a67b5204395ff24e5d
kvanst <kvanst3@gmail.com>
2021-06-05 18:33:03 +0200

    capcha hindering - cannot  progress

--- selenium/linkedin.py
+++ selenium/linkedin.py
@@ -10,7 +10,7 @@ driver.get("https://www.linkedin.com/jobs/search/?f_AL=true&geoId=91000005&keywo
 time.sleep(1)
 sign_in = driver.find_element_by_class_name('cta-modal__primary-btn')
 sign_in.click()
-time.sleep(1)
+time.sleep(1.2)
 
 email_field = driver.find_element_by_id("username")
 email_field.send_keys(os.environ.get('KV_EMAIL'))
@@ -22,23 +22,36 @@ time.sleep(1)
 job_list = driver.find_elements_by_css_selector('.jobs-search-results .occludable-update')
 
 for job in job_list:
+    # time.sleep(0.5)
     job.click()
+    time.sleep(1)
 
     try:
         apply_button = driver.find_element_by_css_selector('.jobs-s-apply .jobs-apply-button')
         apply_button.click()
+        time.sleep(0.5)
         next_button = driver.find_element_by_css_selector('footer button')
         next_button.click()
+        time.sleep(0.5)
         cv_button = driver.find_elements_by_css_selector('footer button')[1]
         cv_button.click()
-        follow_checker = driver.find_element_by_id('follow-company-checkbox')
+        time.sleep(0.5)
+        follow_checker = driver.find_element_by_xpath('//*[@id="ember489"]/div/div[2]/footer/div[1]/label')
         follow_checker.click()
+        time.sleep(0.5)
         submit_button = driver.find_elements_by_css_selector('footer button')[1]
         submit_button.click()
+        time.sleep(1)
         driver.find_element_by_css_selector('button').click()
-    except:
-        driver.find_element_by_css_selector('button').click()
-        driver.find_elements_by_css_selector('footer button')[1]
-        driver.find_elements_by_css_selector(".artdeco-modal--layer-confirmation button")[2].click()
+
+    except Exception:
+        try:
+            driver.find_element_by_css_selector('button').click()
+            time.sleep(0.5)
+            driver.find_elements_by_css_selector('footer button')[1]
+            time.sleep(0.5)
+            driver.find_elements_by_css_selector(".artdeco-modal--layer-confirmation button")[2].click()
+        except Exception:
+            continue
 
 driver.quit() 
