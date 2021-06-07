9cb0b4ab3c6561ad06a18c30f8cbaebda33751f2
kvanst <kvanst3@gmail.com>
2021-06-07 21:08:27 +0200

    send tweet function

--- selenium/twitter_bot.py
+++ selenium/twitter_bot.py
@@ -17,13 +17,35 @@ class InternetSpeedTwitterBot:
         self.driver.find_element_by_id('_evidon-banner-acceptbutton').click()
         self.driver.find_element_by_class_name('start-text').click()
         time.sleep(60)
-        # self.driver.find_element_by_css_selector('.modal .close-btn').click()
+
         self.down = self.driver.find_element_by_class_name('result-item-download').text.split('\n')[1]
         self.up = self.driver.find_element_by_class_name('result-item-upload').text.split('\n')[1]
-        self.driver.quit()
+        # self.driver.quit()
 
     def tweet_at_provider():
-        pass
+        self.driver.get("https://twitter.com/login")
+
+        time.sleep(2)
+        # #this needs to be adapted to ff (maybe)
+        # email = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[1]/label/div/div[2]/div/input')
+        # password = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[2]/label/div/div[2]/div/input')
+
+        # email.send_keys(TWITTER_EMAIL)
+        # password.send_keys(TWITTER_PASSWORD)
+        # time.sleep(2)
+        # password.send_keys(Keys.ENTER)
+
+        # time.sleep(5)
+        # tweet_compose = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div[1]/div/div/div/div[2]/div/div/div/div')
+
+        # tweet = f"Hey Internet Provider, why is my internet speed {self.down}down/{self.up}up when I pay for {PROMISED_DOWN}down/{PROMISED_UP}up?"
+        # tweet_compose.send_keys(tweet)
+        # time.sleep(3)
+
+        # tweet_button = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[4]/div/div/div[2]/div[3]')
+        # tweet_button.click()
+
+        # time.sleep(2)
 
 
 ist = InternetSpeedTwitterBot()
