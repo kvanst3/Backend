a71f9e4c67cae42b3603b9fd102a565ea797b4a0
kvanst <kvanst3@gmail.com>
2021-06-07 20:28:10 +0200

    geting up and down speed

--- selenium/twitter_bot.py
+++ selenium/twitter_bot.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
 class InternetSpeedTwitterBot:
 
@@ -12,6 +13,19 @@ class InternetSpeedTwitterBot:
 
     def get_internet_speed(self):
         self.driver.get('https://www.speedtest.net/')
+        time.sleep(2)
+        self.driver.find_element_by_id('_evidon-banner-acceptbutton').click()
+        self.driver.find_element_by_class_name('start-text').click()
+        time.sleep(60)
+        # self.driver.find_element_by_css_selector('.modal .close-btn').click()
+        self.down = self.driver.find_element_by_class_name('result-item-download').text.split('\n')[1]
+        self.up = self.driver.find_element_by_class_name('result-item-upload').text.split('\n')[1]
 
     def tweet_at_provider():
         pass
+
+
+ist = InternetSpeedTwitterBot()
+ist.get_internet_speed()
+print(ist.down)
+print(ist.up)
