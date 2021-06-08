8db6670a46f1e5a2f0bef32f6545d7177d7b1864
kvanst <kvanst3@gmail.com>
2021-06-08 10:44:22 +0200

    form set up complete

--- selenium/zillow.py
+++ selenium/zillow.py
@@ -4,9 +4,9 @@ import time
 
 driver_path = "/home/k/Desktop/Python_projects/geckodriver"
 url = 'https://docs.google.com/forms/d/e/1FAIpQLSc1Xg2V7BLVLDWs5PYWrufv3NrfjLeY_bVTfuKEkBsJBAQSHg/viewform?usp=sf_link'
-self.driver = webdriver.Firefox(executable_path=driver_path)
+driver = webdriver.Firefox(executable_path=driver_path)
 
-driver.get('url')
+driver.get(url)
 print('r')
 
 driver.quit()
