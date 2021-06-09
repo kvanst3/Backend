3bc1ec46363fcad962cc404f3c056e6d39b79c61
kvanst <kvanst3@gmail.com>
2021-06-09 18:48:02 +0200

    decorators - wrapper function usage

--- decorator/main.py
+++ decorator/main.py
@@ -1,14 +1,25 @@
 import time
-current_time = time.time()
-print(current_time)
+# current_time = time.time()
+# print(current_time)
 
-def speed_calc_decorator():
-    pass
+def speed_calc_decorator(function):
+    def wrapper_function():
+        t1 = time.time()
+        function()
+        t2 = time.time()
+        print(f"{function.__name__} run speed: {t2 - t1}s")
+    return wrapper_function
 
+@speed_calc_decorator
 def fast_function():
     for i in range(10000000):
         i * i
-        
+
+@speed_calc_decorator     
 def slow_function():
     for i in range(100000000):
         i * i
+
+
+fast_function()
+slow_function()
