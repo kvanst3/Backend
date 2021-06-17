33668373915fc9551ae264e3bcdfbff2ee6c0057
kvanst <kvanst3@gmail.com>
2021-06-17 18:43:29 +0200

    website template to flask project set up

---
+++ flask/name_card/server.py
@@ -0,0 +1,13 @@
+from os import name
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html", name=name)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
