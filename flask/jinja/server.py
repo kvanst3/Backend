46188aaefe0e6a97de4b65b0adf9868a570f1837
kvanst <kvanst3@gmail.com>
2021-06-18 16:25:10 +0200

    api call

--- flask/jinja/server.py
+++ flask/jinja/server.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 from os import name
 from random import randint
 from datetime import datetime as dt
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -11,6 +13,16 @@ def home():
     random_num = randint(1,10)
     current_year = dt.now().year
     return render_template("index.html", name=name, num=random_num, current_year=current_year)
+
+@app.route("/guess/<fname>")
+def guess(fname):
+    response = requests.get(f"https://api.genderize.io?name={fname}")
+    result = response.json()
+    gender = result["gender"]
+    response = requests.get(f"https://api.agify.io?name={fname}")
+    result = response.json()
+    age = result["age"]
+    return render_template("guess.html", gender=gender, fname=fname.capitalize(), age=age)
 
 
 if __name__ == "__main__":
