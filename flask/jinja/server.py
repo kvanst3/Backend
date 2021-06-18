9cd29d97b9dd6823adad390b5d48c82c85d79d92
kvanst <kvanst3@gmail.com>
2021-06-18 15:55:05 +0200

    [assign dynamic elements into html

--- flask/jinja/server.py
+++ flask/jinja/server.py
@@ -1,13 +1,16 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 from os import name
+from random import randint
+from datetime import datetime as dt
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html", name=name)
+    random_num = randint(1,10)
+    current_year = dt.now().year
+    return render_template("index.html", name=name, num=random_num, current_year=current_year)
 
 
 if __name__ == "__main__":
