2e4f3e53b5d4bb525857b14ac2859cde3f62f576
kvanst <kvanst3@gmail.com>
2021-06-23 14:52:46 +0200

    Allow for deletion

--- SQLite/movie_project/main.py
+++ SQLite/movie_project/main.py
@@ -104,5 +104,14 @@ def edit():
     return render_template('edit.html', movie=movie, form=form)
 
 
+@app.route('/delete')
+def delete():
+    movie_id = request.args.get('id')
+    movie_to_delete = Movie.query.get(movie_id)
+    db.session.delete(movie_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
+    
+
 if __name__ == '__main__':
     app.run(debug=True)
