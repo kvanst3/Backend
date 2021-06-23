4aaa99cbc80fc12b3989d0a1f55ff7aa9aac501d
kvanst <kvanst3@gmail.com>
2021-06-23 11:51:06 +0200

    display movies on front page

--- SQLite/movie_project/main.py
+++ SQLite/movie_project/main.py
@@ -5,6 +5,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
+import os
 
 DB_URI = 'sqlite:///movies.db'
 
@@ -12,17 +13,28 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap(app)
 
-app.config['DB_URI'] = DB_URI
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class Movie(db.model):
-    id = 
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    rating = db.Column(db.Float, nullable=True)
+    ranking = db.Column(db.Integer, nullable=True)
+    review = db.Column(db.String(250), nullable=True)
+    img_url = db.Column(db.String(250), nullable=False)
+
+
+if not os.path.isfile(DB_URI):
+    db.create_all()
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", all_movies=Movie.query.all())
 
 
 if __name__ == '__main__':
