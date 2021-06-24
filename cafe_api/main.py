5139937fa1dffde63289c9586a9ad5d6f6c96250
kvanst <kvanst3@gmail.com>
2021-06-24 15:55:40 +0200

    create class function to_dict

--- cafe_api/main.py
+++ cafe_api/main.py
@@ -25,6 +25,21 @@ class Cafe(db.Model):
     coffee_price = db.Column(db.String(250), nullable=True)
 
 
+    def to_dict(self):
+        #Method 1. 
+        dictionary = {}
+        # Loop through each column in the data record
+        for column in self.__table__.columns:
+            #Create a new dictionary entry;
+            # where the key is the name of the column
+            # and the value is the value of the column
+            dictionary[column.name] = getattr(self, column.name)
+        return dictionary
+    
+        # #Method 2. Altenatively use Dictionary Comprehension to do the same thing.
+        # return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -34,19 +49,20 @@ def random():
     num = db.session.query(Cafe).count()
     random_cafe = db.session.query(Cafe).get(randint(1 ,num))
     print(random_cafe)
-    return jsonify(
-        id=random_cafe.id,
-        name=random_cafe.name,
-        map_url=random_cafe.map_url,
-        img_url=random_cafe.img_url,
-        location=random_cafe.location,
-        seats=random_cafe.seats,
-        has_toilet=random_cafe.has_toilet,
-        has_wifi=random_cafe.has_wifi,
-        has_sockets=random_cafe.has_sockets,
-        can_take_calls=random_cafe.can_take_calls,
-        coffee_price=random_cafe.coffee_price
-    )
+    return jsonify(cafe=random_cafe.to_dict())
+    # return jsonify(
+    #     id=random_cafe.id,
+    #     name=random_cafe.name,
+    #     map_url=random_cafe.map_url,
+    #     img_url=random_cafe.img_url,
+    #     location=random_cafe.location,
+    #     seats=random_cafe.seats,
+    #     has_toilet=random_cafe.has_toilet,
+    #     has_wifi=random_cafe.has_wifi,
+    #     has_sockets=random_cafe.has_sockets,
+    #     can_take_calls=random_cafe.can_take_calls,
+    #     coffee_price=random_cafe.coffee_price
+    # )
 ## HTTP GET - Read Record
 
 ## HTTP POST - Create Record
