1b3d1f7b944b84fcd30c904282910752d763d764
kvanst <kvanst3@gmail.com>
2021-06-25 11:06:25 +0200

    add update route

--- cafe_api/main.py
+++ cafe_api/main.py
@@ -80,11 +80,37 @@ def search():
         )
     else:
         return jsonify(error={"Not Found": "Sorry, we don't have a cafe at that location."})
-## HTTP POST - Create Record
 
-## HTTP PUT/PATCH - Update Record
 
-## HTTP DELETE - Delete Record
+@app.route("/add", methods=["POST"])
+def post_new_cafe():
+    new_cafe = Cafe(
+        name=request.form.get("name"),
+        map_url=request.form.get("map_url"),
+        img_url=request.form.get("img_url"),
+        location=request.form.get("location"),
+        has_sockets=bool(request.form.get("sockets")),
+        has_toilet=bool(request.form.get("toilet")),
+        has_wifi=bool(request.form.get("wifi")),
+        can_take_calls=bool(request.form.get("calls")),
+        seats=request.form.get("seats"),
+        coffee_price=request.form.get("coffee_price"),
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+    return jsonify(response={"success": "Successfully added the new cafe."})
+
+@app.route("/update-price/<int:id>", methods=['PATCH'])
+def update(id):
+    cafe = db.session.query(Cafe).get(id)
+    if cafe:
+        new_price = request.form.get("new_price")
+        cafe.coffee_price = f"£{new_price}"
+        db.session.commit()
+        return jsonify(response={"success": "Successfully updated the price."}), 200
+    else:
+        return jsonify(error={"Not Found": "Sorry, we don't have a coffee for that id"}), 404
+
 
 
 if __name__ == '__main__':
